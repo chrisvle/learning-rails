72923d71e412d5c3205c2d1552fa2eeafdceb113
Chris Le <chrisvle@berkeley.edu>
2015-10-30 13:55:58 -0700

    I am a pokemon master

--- hw4/app/controllers/dogs_controller.rb
+++ hw4/app/controllers/dogs_controller.rb
@@ -9,7 +9,9 @@ class DogsController < ApplicationController
 
   def create
     Dog.create dog_params
-    redirect_to :dogs
+    # redirect_to :dogs
+    redirect_to dogs_path
+    # redirect_to '/dogs'
   end
 
   def dog_params
